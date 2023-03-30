fe906423c87b2d39a52d3522e3693091d137a243
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-31 02:54:36 +0600

    Added more features🔥

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,13 +1,24 @@
 const { Router } = require("express");
-const { getToDo, saveToDo } = require("../controllers/ToDoController");
 
-const router = Router();
-
-router.get("/", getToDo);
-router.post("/save", saveToDo);
+const {
+	getToDo,
+	saveToDo,
+	deleteToDo,
+	updateToDo
+} = require("../controllers/ToDoController");
 
 // router.get("/", (req, res) => {
 // 	res.json({ message: "Hi there..." });
 // });
+
+const router = Router();
+
+router.get("/", getToDo);
+
+router.post("/save", saveToDo);
+
+router.post("/update", updateToDo);
+
+router.post("/delete", deleteToDo);
 
 module.exports = router;
