97b92821b184f87cef55c512ce51f8bea53a5d6c
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-31 12:45:53 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const cors = require("cors");
 
-const routes = require("./routes/todoRoute");
+const routes = require("./routes/todoRoute.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
