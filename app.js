4cdc7faefc2e47a9eca16092c6dcc37a7600b565
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-31 12:17:12 +0600

    Update

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+
 const cors = require("cors");
+
 const routes = require("./routes/ToDoRoute");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
 
 mongoose
 	.connect(process.env.MONGO_URL, {
@@ -15,11 +20,8 @@ mongoose
 	.then(() => console.log("Connected To MongoDB..."))
 	.catch((err) => console.log(err));
 
-app.use(express.json());
-app.use(cors());
-
 app.use("/", routes);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port By Elcho${PORT}`);
 });
